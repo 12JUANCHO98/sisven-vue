3c8e05133456ba3870c531c579c0c51fa3f6fcec
Juan Quiñonez <juandavid9812@gmail.com>
2025-05-20 20:21:18 -0500

    Implementar página de listado de compradores y definir interfaz de Customer

--- src/customers/pages/index.vue
+++ src/customers/pages/index.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="d-flex justify-content-between align-items-center">
             <h1>Listado de compradores</h1>
-            <button v-if="createModal" type="button" class="btn btn-success" @click="createModal.open()">
+            <button type="button" class="btn btn-success">
                 <FontAwesomeIcon :icon="faPlus" />
             </button>
         </div>
@@ -31,11 +31,11 @@
                     <td>{{ customer.phone_number }}</td>
                     <td>{{ customer.email }}</td>
                     <td class="d-flex justify-content-evenly gap-2">
-                        <button type="button" class="btn btn-danger" @click="deleteRegister(customer.id)">
+                        <button type="button" class="btn btn-danger">
                             <i class="fa fa-trash" aria-hidden="true"></i>
                             <FontAwesomeIcon :icon="faTrash" />
                         </button>
-                        <button v-if="editModal" type="button" class="btn btn-warning" @click="editRegister(customer)">
+                        <button type="button" class="btn btn-warning">
                             <FontAwesomeIcon :icon="faPencil" />
                         </button>
                     </td>
@@ -43,12 +43,6 @@
             </tbody>
         </table>
     </div>
-    <Modal title="Crear comprador" ref="createModal">
-        <CreateForm @cancel="createModal!.close()" @save="update()" />
-    </Modal>
-    <Modal title="Editar comprador" ref="editModal">
-        <EditForm @cancel="editModal!.close()" @save="update()" :info="infoEdit!"/>
-    </Modal>
 </template>
 
 <script setup lang="ts">
@@ -56,16 +50,12 @@ import { onMounted, ref } from 'vue';
 import { Customer } from '@/customers/types';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faTrash, faPencil, faPlus } from '@fortawesome/free-solid-svg-icons'
 
-import Modal from '@/components/Modal.vue';
-import CreateForm from '@/customers/components/createForm.vue';
-import EditForm from '@/customers/components/editForm.vue';
-import { confirmSucess } from '@/util';
+import {faTrash, faPencil, faPlus} from '@fortawesome/free-solid-svg-icons'
 
 const customers = ref<Customer[]>([]);
 
-const getCustomers = () => {
+onMounted(() => {
     axios.get('http://127.0.0.1:8000/api/customers')
         .then((response) => {
             customers.value = response.data;
@@ -73,37 +63,8 @@ const getCustomers = () => {
         .catch((error) => {
             console.error(error);
         });
-}
-
-onMounted(() => {
-    getCustomers()
 })
 
-const deleteRegister = (id: number) => {
-    axios.delete(`http://127.0.0.1:8000/api/customers/${id}`)
-        .then((response) => {
-            customers.value = customers.value.filter(customer => customer.id !== id);
-            confirmSucess();
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-}
-
-
-const createModal = ref<InstanceType<typeof Modal>>();
-
-const editModal = ref<InstanceType<typeof Modal>>();
-const infoEdit = ref<Customer>();
-
-const editRegister = (info: Customer) => {
-    infoEdit.value = info;
-    editModal.value!.open()
-}
-    
-const update = () => {
-    getCustomers()
-    createModal.value?.close()
-    editModal.value?.close()
-}
 </script>
+
+<style scoped></style>
